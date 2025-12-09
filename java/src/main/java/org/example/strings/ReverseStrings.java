309d3ce4a4942a18ef92561599f25f8adf7807ad
Matheus Alves Pereira <matt.alpe.dev@gmail.com>
2025-12-09 18:25:33 -0300

    letcode(557) java improved version

--- java/src/main/java/org/example/strings/ReverseStrings.java
+++ java/src/main/java/org/example/strings/ReverseStrings.java
@@ -1,31 +1,25 @@
 package org.example.strings;
 
 public class ReverseStrings {
-    // https://leetcode.com/problems/reverse-words-in-a-string-iii/description/
+    // https://leetcode.com/problems/reverse-words-in-a-string-iii/description/
     public static String reverseWords(String s) {
-        int n = s.length();
-        int left = 0;
-        int right = 0;
         char[] chars = s.toCharArray();
 
-        while (right <= n) {
-            if (right < n && chars[right] != ' ') {
-                right++;
-            } else {
-                int rightPoint = right;
-                while (left < rightPoint) {
-                    rightPoint--;
-                    char tmp = chars[left];
-                    chars[left] = chars[rightPoint];
-                    chars[rightPoint] = tmp;
-                    left++;
-                }
-
-                right++;
-                left = right;
+        for (int left = 0, right = 0; right <= chars.length; right++) {
+            if (right == chars.length || chars[right] == ' ') {
+                reverse(chars, left, right - 1);
+                left = right + 1;
             }
         }
 
         return String.valueOf(chars);
+    }
+
+    private static void reverse(char[] chars, int start, int end) {
+        while (start < end) {
+            char tmp = chars[start];
+            chars[start++] = chars[end];
+            chars[end--] = tmp;
+        }
     }
 }
