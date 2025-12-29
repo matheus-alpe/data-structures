2ab1bef9ade044aa7ba70303c675eada1a839624
Matheus Alves Pereira <matt.alpe.dev@gmail.com>
2025-12-29 11:27:26 -0300

    letcode(219) java

--- java/src/main/java/org/example/sliding_window/ContainsDuplicate.java
+++ java/src/main/java/org/example/sliding_window/ContainsDuplicate.java
@@ -1,4 +1,21 @@
 package org.example.sliding_window;
 
 public class ContainsDuplicate {
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        int left = 0;
+        int right = 0;
+        int max = nums.length - 1;
+
+        while (right < max) {
+            right++;
+            int diff = Math.abs(left - right);
+            if (nums[left] == nums[right] && diff <= k) return true;
+
+            if (diff >= k || right == max) {
+                right = ++left;
+            }
+        }
+
+        return false;
+    }
 }
