ff82148042c6bdde26c97278aba1daf3c7ffe2c8
Matheus Alves Pereira <matt.alpe.dev@gmail.com>
2026-01-22 18:54:42 -0300

    feat: dijkstra

--- java/src/main/java/org/example/algorithms/graph/Dijkstra.java
+++ java/src/main/java/org/example/algorithms/graph/Dijkstra.java
@@ -1,48 +1,37 @@
 package org.example.algorithms.graph;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
 
 public class Dijkstra {
 
     public HashMap<String, Integer> dijkstra(HashMap<String, HashMap<String, Integer>> graph, String start) {
         HashMap<String, Integer> distances = new HashMap<>();
-        List<String> visited = new ArrayList<>();
+        PriorityQueue<Map.Entry<String, Integer>> visited = new PriorityQueue<>(Map.Entry.comparingByValue());
 
         for (String node : graph.keySet()) {
             distances.put(node, Integer.MAX_VALUE);
         }
         distances.put(start, 0);
+        visited.add(Map.entry(start, 0));
 
-        while (visited.size() < graph.size()) {
-            String currentNode = getClosestNode(distances, visited);
-            visited.add(currentNode);
+        while (!visited.isEmpty()) {
+            Map.Entry<String, Integer> current = visited.poll();
+            String currentNode = current.getKey();
+            int currentDist = current.getValue();
+
+            if (currentDist > distances.get(currentNode)) continue;
 
             for (String neighbor : graph.get(currentNode).keySet()) {
-                if (!visited.contains(neighbor)) {
-                    int newDist = distances.get(currentNode) + graph.get(currentNode).get(neighbor);
-                    if (newDist < distances.get(neighbor)) {
-                        distances.put(neighbor, newDist);
-                    }
+                int newDist = currentDist + graph.get(currentNode).get(neighbor);
+                if (newDist < distances.get(neighbor)) {
+                    distances.put(neighbor, newDist);
+                    visited.add(Map.entry(neighbor, newDist));
                 }
             }
         }
 
         return distances;
-    }
-
-    private String getClosestNode(HashMap<String, Integer> distances, List<String> visited) {
-        String closestNode = null;
-        int closestDistance = Integer.MAX_VALUE;
-
-        for (String node : distances.keySet()) {
-            if (!visited.contains(node) && distances.get(node) < closestDistance) {
-                closestDistance = distances.get(node);
-                closestNode = node;
-            }
-        }
-
-        return closestNode;
     }
 }
